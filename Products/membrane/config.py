8dfcfb3b31c3d992eb2c9d48584b278b2ab46e4a
Rob Miller <ra@burningman.com>
2006-04-08 00:19:30 +0000

    * adding FILTERED_ROLES
    svn path=/membrane/trunk/; revision=21922

--- Products/membrane/config.py
+++ Products/membrane/config.py
@@ -8,3 +8,5 @@ INSTALL_TEST_TYPES = True
 CATEGORY_ANNOTATIONS_KEY = 'Products.membrane.category_map'
 STATUS_CATEGORY_SET = 'membrane_workflow_states'
 ACTIVE_STATUS_CATEGORY = 'active'
+
+FILTERED_ROLES = ('Anonymous', 'Authenticated')
