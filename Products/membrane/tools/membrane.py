a4f8d3acb5644de3d183896d5e3e19a0f5c596af
Helge Tesdal <tesdal@jarn.com>
2008-02-16 12:30:10 +0000

    Made object_implements 2000 times faster
    svn path=/membrane/trunk/; revision=58797

--- Products/membrane/tools/membrane.py
+++ Products/membrane/tools/membrane.py
@@ -32,34 +32,73 @@ from Products.membrane.config import TOOLNAME
 from Products.membrane.events import MembraneTypeRegisteredEvent
 from Products.membrane.events import MembraneTypeUnregisteredEvent
 
+from zope.component import getGlobalSiteManager
 # Use extensible object wrapper to always list the interfaces
+error = ['Products.membrane.interfaces.user.IMembraneUserChanger', 'Products.membrane.interfaces.user.IMembraneUserDeleter', 'Products.membrane.interfaces.user.IMembraneUserRoles']
 def object_implements(object, portal, **kw):
-    # XXX this is NOT very efficient  :-(
+
     def getDottedName(iface):
         # have to do this b/c z2->z3 bridges don't play well
         # w/ __identifier__
         return "%s.%s" % (iface.__module__, iface.__name__)
     
-    res = {}
-    for iface in providedBy(object).flattened():
-        res[getDottedName(iface)] = iface
-    direct = res.values()
-    for iface in direct:
-        for adapter_reg in getRequiredAdapters(iface):
-            # avoid checking 'named' adapters
-            if getattr(adapter_reg, 'name', None):
-                continue
-            adaptable_iface = adapter_reg.provided
-            adapting_from = [i for i in adapter_reg.required if i is not None]
-            skip = False
-            if len(adapting_from) > 1:
-                # only support multiadapters that this object can satisfy alone
-                for i in adapting_from:
-                    if not i in direct:
-                        skip = True
-            if adaptable_iface is not None and not skip:
-                res[getDottedName(adaptable_iface)] = adaptable_iface
-    return res.keys()
+    try:
+        gsm = getGlobalSiteManager()
+        extendors = gsm.adapters._v_lookup._extendors
+
+        def lookup(components, req, result, i, l):
+            tmp = {}
+            if i < l:
+                for required in req.keys():
+                    comps = components.get(required)
+                    if comps:
+                        lookup(comps, req, result, i+1, l)
+            else:
+                for iface,a in components.items():
+                    if [x for x in a.keys() if not x]: # Filter named adapters, unnamed got u''
+                        result[iface] = None
+            result.update(tmp)
+
+        res = {}
+        for iface in providedBy(object).flattened():
+            res[iface] = None
+            # Also look for adapters to parent interfaces __sro__
+            #for parent in iface.__sro__:
+            #    res[parent] = None
+
+        tmp = {}
+
+        adapters = gsm.adapters._adapters
+        for i in range(len(adapters)):
+            order = adapters[i]
+            lookup(order, res, tmp, 0, i)
+
+        res.update(tmp)
+        return [getDottedName(h) for h in res.keys()]
+
+    except (AttributeError,IndexError,TypeError,ValueError):
+        # Fallback in case there is a problem with the fast version
+
+        res = {}
+        for iface in providedBy(object).flattened():
+            res[getDottedName(iface)] = iface
+        direct = res.values()
+        for iface in direct:
+            for adapter_reg in getRequiredAdapters(iface):
+                # avoid checking 'named' adapters
+                if getattr(adapter_reg, 'name', None):
+                    continue
+                adaptable_iface = adapter_reg.provided
+                adapting_from = [i for i in adapter_reg.required if i is not None]
+                skip = False
+                if len(adapting_from) > 1:
+                    # only support multiadapters that this object can satisfy alone
+                    for i in adapting_from:
+                        if not i in direct:
+                            skip = True
+                if adaptable_iface is not None and not skip:
+                    res[getDottedName(adaptable_iface)] = adaptable_iface
+        return res.keys()
 
 registerIndexableAttribute('object_implements', object_implements)
 
