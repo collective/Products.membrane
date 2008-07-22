8b78bcc6987dfbf7404e808029762aeab1b820af
Andreas Zeidler <witsch@plone.org>
2008-07-22 13:53:28 +0000

    update package setup
    svn path=/Products.membrane/trunk/; revision=68775

--- setup.py
+++ setup.py
@@ -1,33 +1,42 @@
 from setuptools import setup, find_packages
-import os
+from os.path import join
 
-version = '1.1b2'
+name = 'Products.membrane'
+path = join(*name.split('.'))
+version = open(join(path, 'version.txt')).read().strip()
+readme = open(join(path, 'README.txt')).read()
+history = open(join('docs', 'HISTORY.txt')).read().replace(name + ' - ', '')
 
-setup(name='Products.membrane',
-      version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='plone membrane member content',
-      author='Rob Miller',
-      author_email='robm@openplans.org',
-      url='http://plone.org/products/membrane',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['Products'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+setup(name = name,
+      version = version,
+      description = 'PluggableAuthService (PAS) plug-ins allowing for the '
+                    'user-related behaviour and data to be obtained from '
+                    'content within a Plone site.',
+      long_description = readme[readme.find('\n\n'):] + '\n' + history,
+      keywords = 'plone membrane member content',
+      author = 'Rob Miller',
+      author_email = 'robm@openplans.org',
+      url = 'http://plone.org/products/membrane',
+      download_url = 'http://pypi.python.org/pypi/Products.membrane/',
+      license = 'GPL',
+      packages = find_packages(exclude=['ez_setup']),
+      namespace_packages = ['Products'],
+      include_package_data = True,
+      platforms = 'Any',
+      zip_safe = False,
+      install_requires = [
           'setuptools',
           # -*- Extra requirements: -*-
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+)
