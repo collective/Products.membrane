06221927f7c7a6c05afddd67606084b469e07c70
Wichert Akkerman <wichert@wiggy.net>
2010-08-30 15:16:31 +0000

    Fix doAddUser API fuckup
    svn path=/Products.membrane/branches/no-avail/; revision=124569

--- Products/membrane/plugins/usermanager.py
+++ Products/membrane/plugins/usermanager.py
@@ -266,7 +266,7 @@ class MembraneUserManager(BasePlugin, Cacheable):
             adder.addUser(login, password)
             return True
         else:
-            raise NotImplemented("IUserAdder utility not available")
+            raise False
 
     def allowPasswordSet(self, login):
         """
