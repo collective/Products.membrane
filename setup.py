71036720aac6ab43ab7cc5eab6eb8df49bf7b038
Maurits van Rees <maurits@vanrees.org>
2013-03-01 14:08:28 +0100

    Preparing release 2.1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import  os.path
 
-version = '2.1.6.dev0'
+version = '2.1.6'
 
 readme = open('README.txt').read()
 history = open(os.path.join('docs', 'changes.rst')).read()
