a7d2a191c4781bcbda06aa2f711dfb680dee03c0
Maurits van Rees <maurits@vanrees.org>
2013-03-31 00:39:07 +0100

    Back to development: 2.1.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import  os.path
 
-version = '2.1.8'
+version = '2.1.9.dev0'
 
 readme = open('README.txt').read()
 history = open(os.path.join('docs', 'changes.rst')).read()
