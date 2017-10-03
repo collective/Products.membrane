9c060cf6e6c5ee6b1703f8c8114621557fb05477
Jens W. Klein <jk@kleinundpartner.at>
2017-10-03 08:41:19 +0200

    pep8 setup

--- setup.py
+++ setup.py
@@ -1,42 +1,44 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 version = '3.0.3.dev0'
 readme = open('README.rst').read()
 history = open('CHANGES.rst').read()
 
-setup(name="Products.membrane",
-      version=version,
-      description="Content-based users and groups for Plone",
-      long_description=readme + '\n' + history,
-      keywords='plone membrane member content remember',
-      author='Rob Miller',
-      author_email='robm@openplans.org',
-      url='https://github.com/collective/Products.membrane',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['Products'],
-      include_package_data=True,
-      platforms='Any',
-      zip_safe=False,
-      install_requires=[
-          "setuptools",
-          "plone.indexer",
-          "Products.CMFPlone >= 4.3",
-      ],
-      extras_require={
-          'test': ['plone.app.testing'],
-      },
-      classifiers=[
-          'Environment :: Web Environment',
-          'Framework :: Plone',
-          'Framework :: Plone :: 4.3',
-          'Framework :: Plone :: 5.0',
-          'Intended Audience :: Other Audience',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
-      ],
-      )
+setup(
+    name="Products.membrane",
+    version=version,
+    description="Content-based users and groups for Plone",
+    long_description=readme + '\n' + history,
+    keywords='plone membrane member content remember',
+    author='Rob Miller',
+    author_email='robm@openplans.org',
+    url='https://github.com/collective/Products.membrane',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    platforms='Any',
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "plone.indexer",
+        "Products.CMFPlone >= 4.3",
+    ],
+    extras_require={
+        'test': ['plone.app.testing'],
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: 5.0',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
