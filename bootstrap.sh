3432512a960641f584e3ccdc9da11ac9f86296c9
ale-rt <alessandro.pisa@gmail.com>
2019-11-19 09:44:22 +0100

    Fix travis build for Plone 5.1 and 5.2

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 rm -r ./lib ./include ./local ./bin
 virtualenv --clear .
 ./bin/pip install -U pip
-./bin/pip install -r https://raw.githubusercontent.com/plone/buildout.coredev/5.2/requirements.txt
+./bin/pip install -r requirements.txt
 ./bin/buildout
