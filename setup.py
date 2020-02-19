c292dd52790f3c8b02fdc090e783776ff6301610
ale-rt <alessandro.pisa@gmail.com>
2020-02-19 08:55:01 +0100

    Preparing release 5.0.0a1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '5.0.dev0'
+version = '5.0.0a1'
 readme = open('README.rst').read()
 history = open('CHANGES.rst').read()
 
