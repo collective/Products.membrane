f3cdf546999a91e8fcbc6580a11a099eeef7d1eb
Peter Mathis <peter.mathis@kombinat.at>
2021-12-06 08:58:25 +0100

    Back to development: 5.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '5.0.3'
+version = '5.0.4.dev0'
 readme = open('README.rst').read()
 history = open('CHANGES.rst').read()
 
