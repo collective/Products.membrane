09963dd6957242be809d14739b7c967bab3b8e38
Gil Forcada Codinachs <gil.gnome@gmail.com>
2026-07-28 10:35:29 +0200

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from setuptools import find_packages
 from setuptools import setup
 
-version = "7.0.1.dev0"
+version = "8.0.0.dev0"
 readme = (Path(".") / "README.rst").read_text()
 history = (Path(".") / "CHANGES.rst").read_text()
 
